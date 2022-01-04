b1b0d585660c4785503012acd76baa7fd5ccd17f
ordegani <oradeganii@gmail.com>
2022-01-04 21:57:21 +0200

    add thunk

--- shecodes/src/actions/index.js
+++ shecodes/src/actions/index.js
@@ -5,7 +5,7 @@ import imdb from "../apis/imdb";
 // (redux thunk not used onle for async functions - but does that a lot)
 // (another use - return fron action creator a function)
 // it is interesig when we return a function. if it's not a function - then we just have reducers with no middlemen, of course
-// when thunk sees we return a function it will call it with dispatch and getState functions:
+// when thunk sees we return a function it will call it with dispatch (=power to change data on the redux side) and getState (=vise versa) functions:
 export const fetchData = () => async (dispatch, getState) => {
     const response = await imdb.get("/");
   
