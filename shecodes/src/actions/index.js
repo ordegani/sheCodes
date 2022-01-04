e9b1b805660f45a16ccba40cb2a1a9108379f4a2
ordegani <oradeganii@gmail.com>
2022-01-04 21:47:55 +0200

    component movies

--- shecodes/src/actions/index.js
+++ shecodes/src/actions/index.js
@@ -1,6 +1,12 @@
 import imdb from "../apis/imdb";
 
-export const fetchData = () => async (dispatch) => {
+
+//avoid the error message "use custom middleware for async actions" by using redux thunk 
+// (redux thunk not used onle for async functions - but does that a lot)
+// (another use - return fron action creator a function)
+// it is interesig when we return a function. if it's not a function - then we just have reducers with no middlemen, of course
+// when thunk sees we return a function it will call it with dispatch and getState functions:
+export const fetchData = () => async (dispatch, getState) => {
     const response = await imdb.get("/");
   
     dispatch({ type: "FETCHED_DATA", payload: response.results });
