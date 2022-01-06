577ee4eda2e0f9f713a12d31c46ea5a66bb62134
ordegani <oradeganii@gmail.com>
2022-01-06 17:04:24 +0200

    add FETCHED_DATA reducer

--- shecodes/src/reducers/index.js
+++ shecodes/src/reducers/index.js
@@ -2,13 +2,12 @@ import { combineReducers } from "redux";
 import moviesReducer from "./moviesReducer";
 
 //seperate the reducers to different files (for good order in big projects)
-
-// export const movies = (state = [], action) => {
-//   if (action.type === "FETCHED_DATA") {
-//     return action.payload;
-//   }
-//   return state;
-// };
+//redusers must be pure!! It can only use it's two parameters (=the previous state and the action value) to decide what to do!
+//for instance - they cannot make an API request
+//data massaging on the two parameters is allowed. I can loop, flter etc` if I use only the two. 
+//numbers and strings are not easily changed , they are not mutable. arrays and objects are. state mutating is forbidden as well, in general - but usually it presents no problem. one exceptional:
+//all reducres created a new array in the memory!
+// see https://www.udemy.com/course/react-redux/learn/lecture/12586898#overview minute 1:30 - yellow and green table
 
 export default combineReducers({
     movies: moviesReducer
