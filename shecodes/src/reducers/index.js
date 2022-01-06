16ca0f20685abea7c60643bb65b83401ca2d77a9
ordegani <oradeganii@gmail.com>
2022-01-06 15:45:24 +0200

    seperate reducers to different files

--- shecodes/src/reducers/index.js
+++ shecodes/src/reducers/index.js
@@ -1,6 +1,15 @@
-export const movies = (state = [], action) => {
-  if (action.type === "FETCHED_DATA") {
-    return action.payload;
-  }
-  return state;
-};
+import { combineReducers } from "redux";
+import moviesReducer from "./moviesReducer";
+
+//seperate the reducers to different files (for good order in big projects)
+
+// export const movies = (state = [], action) => {
+//   if (action.type === "FETCHED_DATA") {
+//     return action.payload;
+//   }
+//   return state;
+// };
+
+export default combineReducers({
+    movies: moviesReducer
+});
