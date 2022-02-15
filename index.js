659997226c231c9f0de6088904b1dcff7d5f0e89
ordegani <oradeganii@gmail.com>
2022-02-15 21:15:56 +0200

    test heroku

--- index.js
+++ index.js
@@ -10,23 +10,23 @@ app.use(cors())
 app.use(express.json())
 const port = 5000;
 
-app.get('/', (req, res) => {
-    res.send('<h1>express (Backend)</h1>')
-})
+// app.get('/', (req, res) => {
+//     res.send('<h1>express (Backend)</h1>')
+// })
 
-app.post('/', (req, res) => {
-    console.log(req.body);
-    let { value } = req.body
-    res.send(value)
-    // add to db 
-})
+// app.post('/', (req, res) => {
+//     console.log(req.body);
+//     let { value } = req.body
+//     res.send(value)
+//     // add to db 
+// })
 
-app.post('/', (req, res) => {
-    console.log(req.body);
+// app.post('/', (req, res) => {
+//     console.log(req.body);
 
-    res.send(req.value)
-    // add to db 
-})
+//     res.send(req.value)
+//     // add to db 
+// })
 
 app.listen(port, () => console.log("connected to port #: " + port));
 
