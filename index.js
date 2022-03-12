1c149c4d29b784a99f06d87a2fe5537e3ac15528
ordegani <oradeganii@gmail.com>
2022-03-12 15:31:34 +0200

    new end points

--- index.js
+++ index.js
@@ -1,18 +1,47 @@
-const express = require("express");
+import blogs from "./schemas/blogs.js";
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
 const app = express();
-const path = require('path')
-const cors = require('cors');
-const { request } = require('http');
-let Blog = require("./schemas/blog.js/index.js");
+// import path from 'path';
+// const { request } = require('http');
 
-const publicDirectory = path.join(__dirname, 'client/build')
-app.use(express.static(publicDirectory))
 
+// const publicDirectory = path.join(__dirname, 'client/build')
+// app.use(express.static(publicDirectory))
 app.use(cors())
 app.use(express.json())
 const port = 5000;
+//config
+const connection_url = 'mongodb+srv://shecodes:shecodes@cluster0.5kxnj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+// API endpoints
+app.post ('/blogs', (req, res)=>{
+  const DBblog = req.body;
 
+  blogs.create(BDblog, (err, data)=>{
+    if (err){
+      res.status(500).send(err)
+    }else{
+      res.status(201).send(data)
+    }
+  })
+})
 
-
+app.get('/blogs', (req, res)=>{
+  blogs.find((err, data)=>{
+    if (err){
+      res.status(500).send(err)
+    }else{
+      res.status(200).send(data)
+    }
+  })
+})
+// DB config
+mongoose.connect(connection_url, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+//listener
 app.listen(port, () => console.log("connected to port #: " + port));
 
