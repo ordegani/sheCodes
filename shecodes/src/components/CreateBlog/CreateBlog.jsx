dcd18417a95cc904fb0b5a984e386d3697cdf67e
ordegani <oradeganii@gmail.com>
2022-07-02 10:56:38 +0300

    comments

--- shecodes/src/components/CreateBlog/CreateBlog.jsx
+++ shecodes/src/components/CreateBlog/CreateBlog.jsx
@@ -1,7 +1,6 @@
 import { React, useState } from "react";
 import axios from "axios";
 import "./CreateBlog.css";
-//TODO edit appearance of "newBlog"
 
 const CreateBlog = () => {
     const [value, setValue] = useState('');
