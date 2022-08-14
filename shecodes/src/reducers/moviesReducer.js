c8bf7817ae1a95a86aead761a38b9300a505e623
ordegani <oradeganii@gmail.com>
2022-08-14 23:28:22 +0300

    removeAll reducer

--- shecodes/src/reducers/moviesReducer.js
+++ shecodes/src/reducers/moviesReducer.js
@@ -2,19 +2,11 @@
   if (action.type === "FETCHED_DATA") {
     return action.payload;
   }
-  //in case it is not type "FETCHED_DATA":
   return state;
 };
 
+export default movies;
+
+
 //usually a switch statement will be used:
 //sometimes the reducer going to care about several types and to handle that we will use switch. here it is not the case.
-
-//export const movies = (state = [], action) => {
-// switch (action.type){
-//     case "FETCHED_DATA":
-//         return action.payload;
-//         default:
-//             return state;
-// }
-// };
-export default movies;
