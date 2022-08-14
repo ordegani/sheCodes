33cb79ab06423c95665141f145b6d4a3ebe5360f
ordegani <oradeganii@gmail.com>
2022-08-14 23:25:55 +0300

    removeAll reducer

--- shecodes/src/reducers/index.js
+++ shecodes/src/reducers/index.js
@@ -1,7 +1,12 @@
 import { combineReducers } from "redux";
 import movies from "./moviesReducer";
 import favoriteReducer from "./favoritesReducer";
-// import deletedReducer from "./deletedReducer";
+
+export default combineReducers({
+    movies: movies,
+    favorites: favoriteReducer,
+});
+
 
 //seperate the reducers to different files (for good order in big projects)
 //redusers must be pure!! It can only use it's two parameters (=the previous state and the action value) to decide what to do!
@@ -10,9 +15,3 @@ import favoriteReducer from "./favoritesReducer";
 //numbers and strings are not easily changed , they are not mutable. arrays and objects are. state mutating is forbidden as well, in general - but usually it presents no problem. one exceptional:
 //all reducres created a new array in the memory!
 // see https://www.udemy.com/course/react-redux/learn/lecture/12586898#overview minute 1:30 - yellow and green table
-
-export default combineReducers({
-    movies: movies,
-    favorites: favoriteReducer,
-    // deleted: deletedReducer,
-});
