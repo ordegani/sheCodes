2c2f15c13132f38028b363eb95b94209edc2d940
ordegani <oradeganii@gmail.com>
2022-08-14 23:27:10 +0300

    removeAll reducer

--- shecodes/src/reducers/favoritesReducer.js
+++ shecodes/src/reducers/favoritesReducer.js
@@ -1,17 +1,13 @@
 const favoriteReducer = (favorites = [], action) => {
     if (action.type === "MOVIE_SAVED_TO_FAVORITES") {
         favorites = favorites.filter((favorite) => favorite.title !== action.payload.title);
-
         return [...favorites, action.payload]
     };
     if (action.type === "MOVIE_DELETED") {
-
         favorites = favorites.filter((favorite) => favorite.title !== action.payload.title);
-
-
         return favorites;
     };
-    if (action.type === "removeAll"){
+    if (action.type === "removeAll") {
         favorites = [];
     }
     return favorites;
