10bb429edfd1cdd603e3abef9eeea9277c84e354
ordegani <oradeganii@gmail.com>
2022-08-14 23:30:45 +0300

    FavoritesList css

--- shecodes/src/components/FavoritesList/FavoriteList.jsx
+++ shecodes/src/components/FavoritesList/FavoriteList.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import Movie from "../Movie/Movie";
 import "../Movies/MoviesList.css";
+import "./FavoriteList.css";
 
 const FavoritesList = (props) => {
     const Flist = () => {
