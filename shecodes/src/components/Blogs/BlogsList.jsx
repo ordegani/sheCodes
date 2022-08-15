b9cdfa1deece51a125e61ee60d9af01ff0527a31
ordegani <oradeganii@gmail.com>
2022-08-16 00:50:40 +0300

    css BlogList fix

--- shecodes/src/components/Blogs/BlogsList.jsx
+++ shecodes/src/components/Blogs/BlogsList.jsx
@@ -15,27 +15,25 @@ const BlogsList = () => {
 
   const Blist = () => {
     return (
-      blogs.slice(0,blogs.length/2).map((blog) => {
+      blogs.slice(0, blogs.length / 2).map((blog) => {
         return (
           <div className="blog">
             <h2>{blog.name}</h2>
             <p>{blog.blog}</p>
           </div>)
       })
-      
-      )
+    )
   }
-  const Blist2 =()=>{
+  const Blist2 = () => {
     return (
-      blogs.slice(blogs.length/2, blogs.length).map((blog) => {
+      blogs.slice(blogs.length / 2, blogs.length).map((blog) => {
         return (
           <div className="blog">
             <h2>{blog.name}</h2>
             <p>{blog.blog}</p>
           </div>)
       })
-      
-      )
+    )
   }
   return (<div className="blogs_listContainer"><div className="inner_blogs_listContainer">{Blist()}</div><div className="inner_blogs_listContainer">{Blist2()}</div></div>);
 }
